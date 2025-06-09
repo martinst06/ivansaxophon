be75cff98475cbbe495e41e960bda96356dda602
martinst06 <mstoyanov@adobe.com>
2025-06-09 22:40:12 +0200

    fix svg

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,14 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'deindj.ch',
+        port: '',
+        pathname: '/wp-content/uploads/**',
+      },
+    ],
   },
 
   // Headers for better security and performance
